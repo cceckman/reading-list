9ef37a69f457a4b349da17c59afad18bba03b723
Charles Eckman <charles@cceckman.com>
2021-08-29 23:27:52 -0700

    Make installable

---
+++ app.js
@@ -0,0 +1,8 @@
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('worker.js')
+    .then((reg) => {
+      console.log('Registration succeeded; got: ', reg.scope);
+    }).catch((error) => {
+      console.log('Registration failed: ', error);
+    });
+}

0c77d8a07c867a636fec4395b6d26bdb13775c37
Charles Eckman <charles@cceckman.com>
2021-09-07 19:53:49 -0700

    Make shareable

--- main.go
+++ main.go
@@ -5,7 +5,16 @@ import (
 	"net/http"
 )
 
+func handleNew(w http.ResponseWriter, r *http.Request) {
+	log.Printf("got share request: %+v", r)
+	w.Write([]byte("share request logged"))
+}
+
 func main() {
-	http.Handle("/reading/admin/", http.StripPrefix("/reading/admin", http.FileServer(http.Dir(""))))
+	http.HandleFunc("/reading/admin/new", handleNew)
+
+	http.Handle("/reading/admin/",
+		http.StripPrefix("/reading/admin", http.FileServer(http.Dir(""))))
+
 	log.Fatal(http.ListenAndServeTLS(":8080", "localhost.pem", "localhost-key.pem", nil))
 }

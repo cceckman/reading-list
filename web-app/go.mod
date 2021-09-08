73515b07f0d74a2456ab8e4cd1a525da8613c798
Charles Eckman <charles@cceckman.com>
2021-09-08 13:37:34 -0700

    Remove sub-path layout; serve at the root

--- web-app/go.mod
+++ web-app/go.mod
@@ -1,3 +1,3 @@
-module github.com/cceckman/reading-list
+module github.com/cceckman/reading-list/web-app
 
 go 1.16
--- web-app/main.go
+++ web-app/main.go
@@ -1,31 +1,62 @@
 package main
 
 import (
+	"io/fs"
 	"log"
 	"net/http"
 	"net/url"
+	"os"
 )
 
-func handleNew(w http.ResponseWriter, r *http.Request) {
+const SERVER_ENV = "READING_LIST_SERVER"
+
+func hasKey(public, private string) bool {
+	cwd, err := os.Getwd()
+	if err != nil {
+		return false
+	}
+	if _, err := fs.Stat(os.DirFS(cwd), public); err != nil {
+		return false
+	}
+	if _, err := fs.Stat(os.DirFS(cwd), private); err != nil {
+		return false
+	}
+	return true
+}
+
+func getTarget() (*url.URL, error) {
+	t := os.Getenv(SERVER_ENV)
+	if t == "" {
+		t = "https://cceckman.com"
+	}
+	return url.Parse(t)
+}
+
+func handleAdd(w http.ResponseWriter, r *http.Request) {
 	queryParams := r.URL.Query()
 	log.Printf("got share request: with query params: %+v", queryParams)
 
-	newUrl := &url.URL{
-		Scheme:   "https",
-		Host:     "cceckman.com",
-		Path:     "reading-list",
-		RawQuery: queryParams.Encode(),
-	}
+	newUrl, _ := getTarget()
+	newUrl.RawQuery = queryParams.Encode()
+
 	// Use 307 Temporary Redirect to preserve the method,
 	// and so the client knows to re-check this page every time.
 	http.Redirect(w, r, newUrl.String(), http.StatusTemporaryRedirect)
 }
 
 func main() {
-	http.HandleFunc("/reading/admin/new", handleNew)
+	if _, err := getTarget(); err != nil {
+		log.Fatal(err)
+	}
 
-	http.Handle("/reading/admin/",
-		http.StripPrefix("/reading/admin", http.FileServer(http.Dir(""))))
+	http.HandleFunc("add", handleAdd)
+	http.Handle("/", http.FileServer(http.Dir("static")))
 
-	log.Fatal(http.ListenAndServeTLS(":8080", "localhost.pem", "localhost-key.pem", nil))
+	if hasKey("localhost.pem", "localhost-key.pem") {
+		log.Print("Listening for HTTPS connection...")
+		log.Fatal(http.ListenAndServeTLS(":8080", "localhost.pem", "localhost-key.pem", nil))
+	} else {
+		log.Print("Listening for HTTPS connection...")
+		log.Fatal(http.ListenAndServe(":8080", nil))
+	}
 }

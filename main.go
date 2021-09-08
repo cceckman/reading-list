45fe380bb1ba0ba3401becea80820561c0e6ecc3
Charles Eckman <charles@cceckman.com>
2021-09-08 12:14:07 -0700

    Convert the 'share' action to a redirect.

--- main.go
+++ main.go
@@ -3,11 +3,22 @@ package main
 import (
 	"log"
 	"net/http"
+	"net/url"
 )
 
 func handleNew(w http.ResponseWriter, r *http.Request) {
-	log.Printf("got share request: %+v", r)
-	w.Write([]byte("share request logged"))
+	queryParams := r.URL.Query()
+	log.Printf("got share request: with query params: %+v", queryParams)
+
+	newUrl := &url.URL{
+		Scheme:   "https",
+		Host:     "cceckman.com",
+		Path:     "reading-list",
+		RawQuery: queryParams.Encode(),
+	}
+	// Use 307 Temporary Redirect to preserve the method,
+	// and so the client knows to re-check this page every time.
+	http.Redirect(w, r, newUrl.String(), http.StatusTemporaryRedirect)
 }
 
 func main() {

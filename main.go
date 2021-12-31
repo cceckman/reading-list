faa374a40f9bf4de7f2d641dc5343575376b5b25
Charles Eckman <charles@cceckman.com>
2021-12-31 12:27:56 -0800

    Serve via tsnet
    Blog post incoming at https://cceckman.com/writing/tsproxy

--- main.go
+++ main.go
@@ -5,16 +5,19 @@ import (
 	"flag"
 	"io/fs"
 	"log"
+	"net"
 	"net/http"
 	"os"
 
 	"github.com/cceckman/reading-list/static"
 	"tailscale.com/client/tailscale"
+	"tailscale.com/tsnet"
 )
 
 var (
-	listen     = flag.String("listen", "[::]:8080", "Port or address:port to listen on")
+	addr       = flag.String("addr", ":443", "Port or address:port to listen on")
 	allowLocal = flag.Bool("allowLocal", false, "Allow serving from the local static/ directory rather than embedded content. Development only.")
+	useTsNet   = flag.Bool("tsnet", true, "Connect directly to Tailscale via tsnet")
 )
 
 func main() {
@@ -32,13 +35,32 @@ func main() {
 	// Root handler: serve from the filesystem.
 	fileHandler := http.FileServer(http.FS(files))
 
-	server := http.Server{
-		Addr:    *listen,
-		Handler: fileHandler,
-		TLSConfig: &tls.Config{
-			GetCertificate: tailscale.GetCertificate,
-		},
+	var ln net.Listener
+	var err error
+	if *useTsNet {
+		s := new(tsnet.Server)
+		ln, err = s.Listen("tcp", *addr)
+	} else {
+		ln, err = net.Listen("tcp", *addr)
 	}
-	log.Print("Starting to run and listen at ", *listen)
-	log.Fatal(server.ListenAndServeTLS("", ""))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	tls := tls.NewListener(ln, &tls.Config{
+		GetCertificate: tailscale.GetCertificate,
+	})
+
+	log.Fatal(http.Serve(tls, http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if *useTsNet {
+			// Check Tailscale authentication
+			who, err := tailscale.WhoIs(r.Context(), r.RemoteAddr)
+			if err != nil {
+				http.Error(w, err.Error(), 500)
+				return
+			}
+			log.Printf("Tailscale-authenticated request from %s at %s", who.UserProfile.LoginName, who.Node.Name)
+		}
+		fileHandler.ServeHTTP(w, r)
+	})))
 }

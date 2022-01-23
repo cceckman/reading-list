1017501e4d1c59a760f9bba73359515b6051ee61
Charles Eckman <charles@cceckman.com>
2022-01-23 10:55:05 -0800

    Add testing script, 'just run it'

---
+++ run.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+ARCH="$(uname -m)"
+case "$(uname -m)" in
+  x86_64) ARCH="amd64";;
+  aarch64) ARCH="arm64";;
+esac
+
+redo -j$(nproc) server/reading-list."$ARCH"
+
+set -x
+export TAILSCALE_USE_WIP_CODE=true
+cd "$(dirname $(realpath $0))"/server
+exec ./reading-list."$ARCH" --allowLocal
